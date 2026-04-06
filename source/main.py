b8d4b42090d574f1eb94050ee7f6b54fde6a845c
xferrin <xaime.ferrin@gmail.com>
2026-04-06 19:17:53 +0200

    main.py

--- source/main.py
+++ source/main.py
@@ -12,5 +12,41 @@ headers = {
 subcategorias = get_subcategories(URL, headers)
 
 
+#Para obtener los productos de cada subcategoría
+
+import requests
+import time
+import random
+import bs4 from BeautifulSoup
+from urlib.parse import urljoin
+
+#Definimos una función que reciba la URL de cada subcategoría, extraiga los productos a través de las diferentes páginas, añada pausas aleatorias para no saturar el servidor y que nos devuleva los productos en formato lista.
+
+def get_product_links(subcat_url, headers):
+    product_links = []
+    current_page = 1
+    while True:
+        url = f"{subcat_url}?page={current_page]"
+        print(f"Descargnado página {current_page} : {url}")
+        time.sleep(random.uniform(0.8, 1.6))
+
+        response = request.get(url, headers=headers)
+        if response.status.code != 200:
+            print("Error al cargar la página:", response.status.code)
+        soup = BeautifulSoup(response.content, "html.parser")
+
+cards = soup.find_all("a", clas_="productt_card")
+if not cards:
+    print("No hay productos. Fin de la paginación"
+    break
+for card in cards:
+    href = card.get("href")
+    full_url= "https://www.naturitas.es", href)
+    product_links.append(full_url)
+
+    current_page += 1
+
+return product_links
+
 
 
