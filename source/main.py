3b9de47f7152f19979be824d477ec7a66efee735
ManuelMerlo <manumerlo@uoc.edu>
2026-04-07 17:49:40 +0200

    Construyo en flujo completo en main. Ha estado funcionando pero nos capa cloudflare.

--- source/main.py
+++ source/main.py
@@ -13,17 +13,6 @@ headers = {
      )
 }
 
-headers = {
-    "User-Agent": (
-        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
-        "AppleWebKit/537.36 (KHTML, like Gecko) "
-        "Chrome/123.0.0.0 Safari/537.36"
-    ),
-    "Accept-Language": "es-ES,es;q=0.9",
-    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
-    "Connection": "keep-alive",
-}
-
 
 # Extraemos las subcategorías de la url inicial: {"name": name, "url": href, "count": count}
 subcategorias = get_subcategories(URL, headers)
